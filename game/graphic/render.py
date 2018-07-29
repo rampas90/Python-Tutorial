bee26cc703fa6686da21bda70dd57a266060e2ff
dongminleeai <kid33629@gmail.com>
2018-07-29 14:43:59 +0900

    tutorial

---
+++ game/graphic/render.py
@@ -0,0 +1,13 @@
+# def render_test():
+#     print ("render")
+
+# from game.sound.echo import echo_test
+# def render_test():
+#     print ("render")
+#     echo_test()
+
+from ..sound.echo import echo_test
+
+def render_test():
+    print ("render")
+    echo_test()
